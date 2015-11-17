75e29c1082d44f6762d84df9cd596a569115fd84
Anthony Best <anthony.best@micronet-inc.com>
2015-11-17 15:37:03 -0700

    Add basic frame code + stubs

---
+++ MCU_TASKS/Sources/USB/frame.h
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stdbool.h>
+typedef struct
+{
+	uint8_t * data;
+	uint32_t data_alloc;
+	uint32_t data_len;
+
+	bool escape_flag;
+	bool data_ready;
+} frame_t;
+
+void frame_reset(frame_t * frame);
+void frame_setbuffer(frame_t * frame, uint8_t * buffer, uint32_t len);
+int frame_process_buffer(frame_t * frame, uint8_t * buffer, uint32_t len);
+
+int frame_encode(uint8_t * s, uint8_t * d, int len);
+bool frame_data_ready(frame_t * frame);
+
