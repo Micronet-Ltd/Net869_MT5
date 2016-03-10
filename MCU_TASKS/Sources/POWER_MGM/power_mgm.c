e955bc9e88ce9d5a81c175c3f409d8d6c6dc8b83
Abid Esmail <abid.esmail@micronet-inc.com>
2016-03-10 14:58:41 -0700

    minor typo in power management and changes braces to improve readability

--- MCU_TASKS/Sources/POWER_MGM/power_mgm.c
+++ MCU_TASKS/Sources/POWER_MGM/power_mgm.c
@@ -50,33 +50,39 @@ void Power_MGM_task (uint32_t initial_data )
 
 	MIC_DEBUG_UART_PRINTF ("\nPower Management Task: Start \n");
 
-	while (1) {
+	while (1)
+	{
 		ADC_sample_input (kADC_POWER_IN);
 		ADC_sample_input (kADC_POWER_VCAP);
 
-		if ((current_time - ignition_last_time) >= IGNITION_TIME_DELAY) {
+		if ((current_time - ignition_last_time) >= IGNITION_TIME_DELAY)
+		{
 			ignition_last_time = current_time;
 			ADC_sample_input (kADC_ANALOG_IN1);
 		}
 
-		if ((current_time - temperature_last_time) >= TEMPERATURE_TIME_DELAY) {
+		if ((current_time - temperature_last_time) >= TEMPERATURE_TIME_DELAY)
+		{
 			temperature_last_time = current_time;
 			ADC_sample_input (kADC_TEMPERATURE);
 		}
 
 		// check cable type - report if not detected (tamper) or not as expected
-		if ((current_time - cable_type_last_time) >= CABLE_TYPE_TIME_DELAY) {
+		if ((current_time - cable_type_last_time) >= CABLE_TYPE_TIME_DELAY)
+		{
 			cable_type_last_time = current_time;
 			ADC_sample_input (kADC_CABLE_TYPE);
 
 			cable_type_voltage = ADC_get_value(kADC_CABLE_TYPE);
 			if ((cable_type_voltage < CABLE_TYPE_MIN_VOLTAGE) ||
-				(cable_type_voltage > CABLE_TYPE_MAX_VOLTAGE) ) {
+				(cable_type_voltage > CABLE_TYPE_MAX_VOLTAGE) )
+			{
 				MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: WARNING: CABLE TYPE is not as expected (current voltage %d mV - expected %d mV\n", cable_type_voltage, CABLE_TYPE_VOLTAGE);
 			}
 		}
 
-		if ((current_time - ext_gpio_last_time) >= EXT_GPIO_TIME_DELAY) {
+		if ((current_time - ext_gpio_last_time) >= EXT_GPIO_TIME_DELAY)
+		{
 			ext_gpio_last_time = current_time;
 			GPIO_sample_all ();
 		}
@@ -93,19 +99,24 @@ void Power_MGM_task (uint32_t initial_data )
 // continue till supercap voltage level reaches upper threshold.
 void Supercap_charge_state (void)
 {
-	uint32_t supercap_voltaeg = ADC_get_value (kADC_POWER_VCAP);
+	uint32_t supercap_voltage = ADC_get_value (kADC_POWER_VCAP);
 
-	if (supercap_voltaeg >= SUPERCAP_MAX_TH) {
+	if (supercap_voltage >= SUPERCAP_MAX_TH)
+	{
 		// send a message only once
 		if (GPIO_DRV_ReadPinInput (POWER_CHARGE_ENABLE) == 1)
-			MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: SUPERCAP full Charged %d mV\n", supercap_voltaeg);
-
+		{
+			MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: SUPERCAP full Charged %d mV\n", supercap_voltage);
+		}
 		GPIO_DRV_ClearPinOutput (POWER_CHARGE_ENABLE);
-	} else if (supercap_voltaeg <= SUPERCAP_MIN_TH) {
+	}
+	else if (supercap_voltage <= SUPERCAP_MIN_TH)
+	{
 		// send a message only once
 		if (GPIO_DRV_ReadPinInput (POWER_CHARGE_ENABLE) == 0)
-			MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: SUPERCAP low %d mV - Start Charging\n", supercap_voltaeg);
-
+		{
+			MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: SUPERCAP low %d mV - Start Charging\n", supercap_voltage);
+		}
 		GPIO_DRV_SetPinOutput (POWER_CHARGE_ENABLE);
 	}
 }
@@ -116,17 +127,22 @@ void Supercap_discharge_state (void)
 {
 	uint32_t power_in_voltage  = ADC_get_value (kADC_POWER_IN);
 
-	if (power_in_voltage <= POWER_IN_SHUTDOWN_TH) {
+	if (power_in_voltage <= POWER_IN_SHUTDOWN_TH)
+	{
 		// send a message only once
 		if (GPIO_DRV_ReadPinInput (POWER_DISCHARGE_ENABLE) == 0)
+		{
 			MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: SUPERCAP start DisCharged (power in voltage is: %d mV)\n", power_in_voltage);
-
+		}
 		GPIO_DRV_SetPinOutput (POWER_DISCHARGE_ENABLE);
-	} else {
+	}
+	else
+	{
 		// send a message only once
 		if (GPIO_DRV_ReadPinInput (POWER_DISCHARGE_ENABLE) == 1)
+		{
 			MIC_DEBUG_UART_PRINTF ("\nPOWER_MGM: SUPERCAP stop DisCharged (power in voltage is: %d mV)\n", power_in_voltage);
-
+		}
 		GPIO_DRV_ClearPinOutput (POWER_DISCHARGE_ENABLE);
 	}
 }
