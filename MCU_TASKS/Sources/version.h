da43a17a1c524e2edd8d133c3d3d67d58d56e244
Abid Esmail <abid.esmail@micronet-inc.com>
2016-03-16 16:48:13 -0600

    Revert "TEMP: Temporarily set CAN_EN low and disable the CAN and J1708 tasks for"
    This reverts commit cc08d47bbcac6427b9d5d53d10ca6eeeae283511.

--- MCU_TASKS/Sources/version.h
+++ MCU_TASKS/Sources/version.h
@@ -11,6 +11,6 @@
 #define FW_VER_BTLD_OR_APP 0x0A /* 0xA : Application, 0xB: Bootloader */
 #define FW_VER_MAJOR 0x0
 #define FW_VER_MINOR 2
-#define FW_VER_BUILD 1
+#define FW_VER_BUILD 0
 
 #endif /* _VERSION_H_ */
