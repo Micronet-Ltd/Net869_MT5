7bc104ac159ec9268b6df5f6fddab15fd9cd6b3e
Abid Esmail <abid.esmail@micronet-inc.com>
2016-05-12 10:00:07 -0600

    Version A.0.D.0

--- MCU_TASKS/Sources/version.h
+++ MCU_TASKS/Sources/version.h
@@ -10,7 +10,7 @@
 
 #define FW_VER_BTLD_OR_APP 0xA /* 0xA : Application, 0xB: Bootloader */
 #define FW_VER_MAJOR 0x0
-#define FW_VER_MINOR 0xC
+#define FW_VER_MINOR 0xD
 #define FW_VER_BUILD 0
 
 #endif /* _VERSION_H_ */
