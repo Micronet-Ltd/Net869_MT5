8e367c1bfa2f96520b73733e75ee5261b0670fe9
Abid <abid.esmail@micronet-inc.com>
2016-06-21 15:56:51 -0600

    ENH: Changing temperature threshold to -25c to 90c

--- MCU_TASKS/Sources/POWER_MGM/power_mgm.h
+++ MCU_TASKS/Sources/POWER_MGM/power_mgm.h
@@ -18,7 +18,7 @@ extern "C"
 #define IGNITION_TURN_ON_TH_DEFAULT			POWER_IN_TURN_ON_TH
 
 /* Conversion from Temperature(c) to mV => 		mV = (temp * 10) + 500 */
-#define TEMPERATURE_MIN_TH		300			 /* mV = (-20 * 10) + 500 */
+#define TEMPERATURE_MIN_TH		250			 /* mV = (-25 * 10) + 500 */
 #define TEMPERATURE_MAX_TH		1400		 /* mV = (90 * 10) + 500 */
 
 void get_ignition_threshold(uint32_t * p_ignition_threshold);
